c5ece67a9dab5d9be945eee5e1f6b1d3917f2148
Natalie <natawerch@gmail.com>
2023-06-01 15:50:36 +0200

    commit

--- src/main/java/homework230523/task2/DateTest.java
+++ src/main/java/homework230523/task2/DateTest.java
@@ -8,14 +8,14 @@ public class DateTest {
 
     public static void main(String[] args) {
         LocalDate birthday = LocalDate.of(1984, 5, 16);
-        String myBirthday = getMyBirthDay(birthday);
+        String myBirthday = getMyBirthDayAsString(birthday);
         System.out.println("День моего рождения: " + myBirthday);
         System.out.println("Является ли дата: " + myBirthday + " пятницей? " + isFriday(birthday));
         System.out.println("Десять лет назад от " + myBirthday + " было " + subtractTenYearsFromBirthday(birthday));
 
     }
 
-    public static String getMyBirthDay(LocalDate date) {
+    public static String getMyBirthDayAsString(LocalDate date) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d MMMM yyyy");
         return date.format(formatter);
 
@@ -23,13 +23,11 @@ public class DateTest {
 
     public static boolean isFriday(LocalDate localDate) {
         DayOfWeek dayOfWeek = localDate.getDayOfWeek();
-        if (dayOfWeek.equals(DayOfWeek.FRIDAY)) {
-            return true;
-        }return false;
+        return DayOfWeek.FRIDAY == dayOfWeek;
     }
 
     public static String subtractTenYearsFromBirthday(LocalDate localDate) {
         LocalDate tenYearsAgo = localDate.minusYears(10);
-        return getMyBirthDay(tenYearsAgo);
+        return getMyBirthDayAsString(tenYearsAgo);
     }
 }
